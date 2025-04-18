c634c2217596773f5049ad4e96bdc30e531fb804
AounSyed <Aoungalaxy@gmail.com>
2025-04-18 17:59:13 -0400

    Completed assignment

--- src/main/java/partA/activity/Activity01_ExplicitImport.java
+++ src/main/java/partA/activity/Activity01_ExplicitImport.java
@@ -1,4 +1,11 @@
 package partA.activity;
+import java.util.Scanner;
 
 public class Activity01_ExplicitImport {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello user, please enter your first name!");
+        String name = input.nextLine();
+        System.out.println("Using the scanner, I was able to store your name. I see your name is " + name);
+    }
 }
--- src/main/java/partA/activity/Activity02_NonExplicitImport.java
+++ src/main/java/partA/activity/Activity02_NonExplicitImport.java
@@ -1,4 +1,12 @@
 package partA.activity;
 
 public class Activity02_NonExplicitImport {
+    public static void main(String args[])
+    {
+        int min = 1;
+        int max = 6;
+        int number = (int)(Math.random()*(max-min+1)+min);
+        System.out.println("Let's play some craps! Throw the dice!");
+        System.out.println("You rolled a: " + number);
+    }
 }
